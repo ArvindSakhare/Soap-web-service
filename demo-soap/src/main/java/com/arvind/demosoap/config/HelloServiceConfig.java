ace5dd6636e304bc8d69e87a567a381f88539d3d
ArvindSakhare <arvindsakhare1@gmail.com>
2019-09-28 20:20:27 +0530

    Implemented WebserviceConfig (ServletRegistration and wsdldefinatin)

--- demo-soap/src/main/java/com/arvind/demosoap/config/HelloServiceConfig.java
+++ demo-soap/src/main/java/com/arvind/demosoap/config/HelloServiceConfig.java
@@ -1,10 +1,35 @@
 package com.arvind.demosoap.config;
 
+import javax.servlet.Servlet;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
+import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
 
 @EnableWs
 @Configuration
 public class HelloServiceConfig {
 
+	@Bean
+	public ServletRegistrationBean<Servlet> messageDispatcherServlet(ApplicationContext context)
+	{
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(context);
+		
+		return new ServletRegistrationBean<Servlet>(servlet,"/ws/*");
+	}
+	
+	@Bean(name = "helloworld")
+	public Wsdl11Definition defaultwsdl11defination()
+	{
+		SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
+		wsdl11Definition.setWsdl(new ClassPathResource("/helloworld.wsdl"));
+		return wsdl11Definition;
+	}
 }
