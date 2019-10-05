a5dafedfacc2f3f67d862373663a6ae3da5c01b3
ArvindSakhare <arvindsakhare1@gmail.com>
2019-10-05 19:26:06 +0530

    Endpoint implemented

--- demo-soap/src/main/java/com/arvind/demosoap/endpoint/HelloEndpoint.java
+++ demo-soap/src/main/java/com/arvind/demosoap/endpoint/HelloEndpoint.java
@@ -1,8 +1,27 @@
 package com.arvind.demosoap.endpoint;
 
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.arvind.demosoap.model.Greeting;
+import com.arvind.demosoap.model.ObjectFactory;
+import com.arvind.demosoap.model.Person;
 
 @Endpoint
 public class HelloEndpoint {
 
+	@PayloadRoot(namespace = "http://codenotfound.com/types/helloworld",
+			localPart = "person")
+	@ResponsePayload
+	public Greeting sayHello(@RequestPayload Person request)
+	{
+		ObjectFactory factory = new ObjectFactory();
+		Greeting response = factory.createGreeting();
+		response.setGreeting("Hello "+ request.getFirstName() + " "+ request.getLastName());
+		
+		return response;
+	}
+	
 }
